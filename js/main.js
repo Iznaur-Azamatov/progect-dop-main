5b04475037b9c24863343b66640089a389ec438a
Azamatov Iznaur <azamatoviznaur@gmail.com>
2023-11-28 03:08:59 +0300

    Added my project

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$('.single-item').slick({
+	infinite: true,
+	slidesToShow: 4,
+	slidesToScroll: 1,
+	responsive: [
+	    {
+	      breakpoint: 768,
+	      settings: {
+	        slidesToShow: 2,
+	      }
+	    },
+	    {
+	      breakpoint: 480,
+	      settings: {
+	        slidesToShow: 1,
+	      }
+	    }
+    ]
+});
